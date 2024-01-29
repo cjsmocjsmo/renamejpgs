98215f14efb7d039b143033f79ba653c070a9c9e
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-28 23:05:54 -0800

    Refactor file reading and writing logic

--- src/main.rs
+++ src/main.rs
@@ -32,9 +32,9 @@ fn main() {
                 
 
                 // open the file in a buffer and read the contents to bytes
-                // let mut buffer = fs::read(&fname).expect("Unable to read file");
-                // // write the bytes to the new location
-                // fs::write(&out_path, &mut buffer).expect("Unable to write file");
+                let mut buffer = fs::read(&fname).expect("Unable to read file");
+                // write the bytes to the new location
+                fs::write(&out_path, &mut buffer).expect("Unable to write file");
                 
 
                 
