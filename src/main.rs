73ba82fc2a7e4613a8d4726f149be4aba598b727
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-28 22:14:20 -0800

    Refactor file name handling and remove inappropriate file names

--- src/main.rs
+++ src/main.rs
@@ -19,27 +19,36 @@ fn main() {
     {
         if e.metadata().unwrap().is_file() {
             let fname = e.path().to_string_lossy().to_string();
-            let middlename;
-            let finalname;
-
-            if fname.contains(" ") {
-                middlename = fname.replace(" ", "_");
+            if fname.contains("pussy") {
+                // remove fname
+                fs::remove_file(&fname).expect("Unable to remove file");
             } else {
-                middlename = fname.clone();
-            };
+                let middlename;
+                let finalname;
 
-            if middlename.contains(".JPG") {
-                finalname = middlename.replace(".JPG", ".jpg");
-            } else if middlename.contains(".jpeg") {
-                finalname = middlename.replace(".jpeg", ".jpg");
-            } else {
-                finalname = middlename;
-            };
+                if fname.contains(" ") {
+                    middlename = fname.replace(" ", "_");
+                } else {
+                    middlename = fname.clone();
+                };
 
-            if finalname != fname {
-                fs::rename(&fname, &finalname).expect("Unable to rename file");
-                println!("Renamed\n{}\nto\n{}", fname, finalname)
-            }
-        };
+                if middlename.contains(".JPG") {
+                    finalname = middlename.replace(".JPG", ".jpg");
+                } else if middlename.contains(".jpeg") {
+                    finalname = middlename.replace(".jpeg", ".jpg");
+                } else if middlename.contains(".PNG") {
+                    finalname = middlename.replace(".PNG", ".png");
+                } else if middlename.contains(".BMP") {
+                    finalname = middlename.replace(".BMP", ".bmp");
+                } else {
+                    finalname = middlename;
+                };
+
+                if finalname != fname {
+                    fs::rename(&fname, &finalname).expect("Unable to rename file");
+                    println!("Renamed\n{}\nto\n{}", fname, finalname)
+                }
+            };
+        }
     }
 }
